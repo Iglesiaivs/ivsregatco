cdf7f91562b8afef961543ae65ce38abe93be6b2
ivsregionalatlantico <ivsregionalatlantico@gmail.com>
2024-11-04 19:50:35 +0000

    three commit

--- public/index.js
+++ public/index.js
@@ -28,25 +28,27 @@ import {
   ref,
   set,
   onValue,
-  query, 
+  query,
   orderByKey,
   get,
   limitToLast,
-  equalTo, 
+  equalTo,
   child,
   update
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-let direccion="", celular="", ciudad="";
+let direccion = "", celular = "", ciudad = "";
 
 //Firebase: Initialize service
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyArK2n6_hj6ijMv5v6c7w924zgtK8G8INI",
-  authDomain: "iglesiaivsregionalatco.firebaseapp.com",
-  projectId: "iglesiaivsregionalatco",
-  storageBucket: "iglesiaivsregionalatco.appspot.com",
-  messagingSenderId: "228956104410",
-  appId: "1:228956104410:web:6ceb3f6bed247d9df3e7e3"
+  apiKey: "AIzaSyCjw1LBpUPDCVfh5jvHN6_MJkm3k-utfYE",
+  authDomain: "ivsiglesiaregatco.firebaseapp.com",
+  databaseURL: "https://ivsiglesiaregatco-default-rtdb.firebaseio.com",
+  projectId: "ivsiglesiaregatco",
+  storageBucket: "ivsiglesiaregatco.firebasestorage.app",
+  messagingSenderId: "892878960949",
+  appId: "1:892878960949:web:93bc8715bc0de5b9cd747c",
+  measurementId: "G-K4L55LN63M"
 });
 
 const auth = getAuth(firebaseApp);
@@ -60,18 +62,18 @@ const logoutButton = document.getElementById("loginout");
 // Obtenemos el botón de cierre de sesión, desde perfil
 const logoutButton2 = document.getElementById("loginout2");
 // Obtenemos el elemento botón
-const login  = document.getElementById("login");
+const login = document.getElementById("login");
 // Obtenemos el elemento botón
 const login2 = document.getElementById("login2");
 
 //Desplegamos el boton: oCULTAR
 logoutButton.style.display = "none";
-login.style.display        = "none";
+login.style.display = "none";
 
 //Ocultamos el texto del modal perfil
 logoutButton2.style.display = "none";
 //Desplegamos el texto del modal perfil
-login2.style.display    = "none";
+login2.style.display = "none";
 // ...          
 
 login.addEventListener("click", (e) => {
@@ -79,7 +81,7 @@ login.addEventListener("click", (e) => {
   signInWithRedirect(auth, provider);
 
   getRedirectResult(auth)
-  .then((result) => {
+    .then((result) => {
       // This gives you a Google Access Token. You can use it to access Google APIs.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
@@ -87,16 +89,16 @@ login.addEventListener("click", (e) => {
       // The signed-in user info.
       const user = result.user;
       console.log(user);
-      
+
       //Desplegamos el boton
-  ////logoutButton.style.display = "block";
+      ////logoutButton.style.display = "block";
       //Ocultamos el boton
-  ////login.style.display   = "none";
+      ////login.style.display   = "none";
 
       //Ocultamos el texto del modal perfil
-  ////logoutButton2.style.display = "block";
+      ////logoutButton2.style.display = "block";
       //Desplegamos el texto del modal perfil
-  ////login2.style.display    = "none";
+      ////login2.style.display    = "none";
       // ...          
 
     })
@@ -114,7 +116,7 @@ login2.addEventListener("click", (e) => {
   signInWithRedirect(auth, provider);
 
   getRedirectResult(auth)
-  .then((result) => {
+    .then((result) => {
       // This gives you a Google Access Token. You can use it to access Google APIs.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
@@ -125,12 +127,12 @@ login2.addEventListener("click", (e) => {
       //Desplegamos el boton
       logoutButton.style.display = "block";
       //Ocultamos el boton
-      login.style.display   = "none";       
-      
+      login.style.display = "none";
+
       //Ocultamos el texto del modal perfil
       logoutButton2.style.display = "block";
       //Desplegamos el texto del modal perfil
-      login2.style.display    = "none";
+      login2.style.display = "none";
       // ...          
 
     })
@@ -153,17 +155,17 @@ logoutButton.addEventListener("click", (e) => {
     //Ocultamos el boton
     logoutButton.style.display = "none";
     //Desplegamos el boton
-    login.style.display   = "block";
+    login.style.display = "block";
 
     //Ocultamos el texto del modal perfil
     logoutButton2.style.display = "none";
     //Desplegamos el texto del modal perfil
-    login2.style.display    = "block";
+    login2.style.display = "block";
     // ...   
 
     //modal perfil: cuenta
     document.getElementById("idcuenta-nombre").innerText = "";
-    document.getElementById("idcuenta-email").innerText  = "";
+    document.getElementById("idcuenta-email").innerText = "";
 
   }).catch((error) => {
     // Mostrar un mensaje de error
@@ -182,17 +184,17 @@ logoutButton2.addEventListener("click", (e) => {
     //Ocultamos el boton
     logoutButton.style.display = "none";
     //Desplegamos el boton
-    login.style.display   = "block";
+    login.style.display = "block";
 
     //Ocultamos el texto del modal perfil
     logoutButton2.style.display = "none";
     //Desplegamos el texto del modal perfil
-    login2.style.display    = "block";
+    login2.style.display = "block";
     // ...        
 
     //modal perfil: cuenta
     document.getElementById("idcuenta-nombre").innerText = "";
-    document.getElementById("idcuenta-email").innerText  = "";
+    document.getElementById("idcuenta-email").innerText = "";
 
   }).catch((error) => {
     // Mostrar un mensaje de error
@@ -200,7 +202,7 @@ logoutButton2.addEventListener("click", (e) => {
     console.log(error);
   });
 });
- 
+
 
 
 // Agrega un evento al botón actuperfil para agregar el subnivel "perfil" a los datos del usuario
@@ -255,67 +257,67 @@ actuperfil.addEventListener("click", () => {
 //AL cambiar el estado de autenticacion
 onAuthStateChanged(auth, (user) => {
   if (user) {
-      const uid    = user.uid;
-      const uname  = user.displayName;
-      const uemail = user.email;
-      let id       = 1;
+    const uid = user.uid;
+    const uname = user.displayName;
+    const uemail = user.email;
+    let id = 1;
 
-      //modal perfil: cuenta
-  ////document.getElementById("idcuenta-nombre").innerText = "Nombre : " + uname;
-  ////document.getElementById("idcuenta-email").innerText  = "Email : "  + uemail;
+    //modal perfil: cuenta
+    ////document.getElementById("idcuenta-nombre").innerText = "Nombre : " + uname;
+    ////document.getElementById("idcuenta-email").innerText  = "Email : "  + uemail;
 
-      //Ocultamos el botón
-  ////login.style.display   = "none";
-      //Desplegamos el botón
-  ////logoutButton.style.display = "block";
+    //Ocultamos el botón
+    ////login.style.display   = "none";
+    //Desplegamos el botón
+    ////logoutButton.style.display = "block";
 
-      //Ocultamos el texto del modal perfil
-  ////logoutButton2.style.display = "block";
-      //Desplegamos el texto del modal perfil
-  ////login2.style.display    = "none";
-      // ...    
+    //Ocultamos el texto del modal perfil
+    ////logoutButton2.style.display = "block";
+    //Desplegamos el texto del modal perfil
+    ////login2.style.display    = "none";
+    // ...    
 
-      const emailEncoded = btoa(uemail); // Codificar el email en Base64
-      const db = getDatabase();
+    const emailEncoded = btoa(uemail); // Codificar el email en Base64
+    const db = getDatabase();
 
-      const dbf = ref(db, 'usuario/idkey:' + emailEncoded);
-      onValue(dbf, (snapshot) => {
-        let data = snapshot.val();
-      
-        if (data !== null) {
-          // Si data no es nulo, significa que hay un valor en el nodo
-          console.log('Hay un valor en el nodo: ......... ');
-          //console.log(data);
-        }
-        else {
-          // Si data es nulo, significa que no hay un valor en el nodo
-          console.log('No hay un valor en el nodo');
-          const path = 'usuario/idkey:' + emailEncoded;
-          // Luego, puedes usar 'path' en tu función set
-          set(ref(db, path), {
-            nombre: uname,
-            email: uemail,
-            key: uid,
-            idrol: 4,
-            idnivel: 2
-          });
-        }
-      });
-      // ...
-  } 
+    const dbf = ref(db, 'usuario/idkey:' + emailEncoded);
+    onValue(dbf, (snapshot) => {
+      let data = snapshot.val();
+
+      if (data !== null) {
+        // Si data no es nulo, significa que hay un valor en el nodo
+        console.log('Hay un valor en el nodo: ......... ');
+        //console.log(data);
+      }
+      else {
+        // Si data es nulo, significa que no hay un valor en el nodo
+        console.log('No hay un valor en el nodo');
+        const path = 'usuario/idkey:' + emailEncoded;
+        // Luego, puedes usar 'path' en tu función set
+        set(ref(db, path), {
+          nombre: uname,
+          email: uemail,
+          key: uid,
+          idrol: 4,
+          idnivel: 2
+        });
+      }
+    });
+    // ...
+  }
   else {
     // User is signed out
 
     //Ocultamos el botón
-  ////logoutButton.style.display = "none";
+    ////logoutButton.style.display = "none";
     //Desplegamos el botón
-  ////login.style.display    = "block";
+    ////login.style.display    = "block";
     // ...
 
     //Ocultamos el texto del modal perfil
-  ////logoutButton2.style.display = "none";
+    ////logoutButton2.style.display = "none";
     //Desplegamos el texto del modal perfil
-  ////login2.style.display    = "block";
+    ////login2.style.display    = "block";
     // ...    
 
 
